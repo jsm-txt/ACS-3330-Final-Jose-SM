8fce2a6d421ea3dbacfae65105379b044ca24810
Jose <hose153@gmail.com>
2021-10-08 00:54:58 -0500

    fixed display

--- src/CharacterDisplay.js
+++ src/CharacterDisplay.js
@@ -1,20 +1,23 @@
 function CharachterDisplay(props){
-  const example = [{name:"omg"},{name:"lol"}]
+  // const example = [{name:"omg"},{name:"lol"}]
   const list = props
-  let display= '';
-  console.log(list)
-  for(let x in list){
-    display += list[x].name + " "
+  const display = [ ]
+  
+  // for(let x in list){
+  //   display += list[x].name + " "
+  // }
+  
+  for (let x in list) {
+    display.push(<div><header>{list[x].name}</header>
+    <small>Heigh: {list[x].height}</small><br></br>
+      <small>Mass:{list[x].mass}</small><br></br>
+      <small>Hair: {list[x].hair_color}</small><br></br>
+      <small>Eye Color:{list[x].eye_color}</small><br></br>
+    </div>)
   }
-  // {list.map(item => (
-  //         <li>{item.name}</li>
-  //       ))}
   return (
     <div>
-      <ul>
-        {display}
-        
-      </ul>
+      {display}
     </div>
   )
 }
